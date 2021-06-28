2d8aacb21f8d0bf9e53eb82f1be04709d4d51121
snowleopardAlexa <alexaslomski@gmail.com>
2021-06-28 16:12:58 -0400

    add content const

--- backpack-script.js
+++ backpack-script.js
@@ -43,10 +43,38 @@ document.body.innerHTML = content;
 // const content = "<h1>${everydayPack.name}</h1>";
 
 // string concatenation - outdated
-const content = "<h1>" + everydayPack.name + "</h1>";
+//const content = "<h1>" + everydayPack.name + "</h1>";
+//document.body.innerHTML = content;
 
-document.body.innerHTML = content;
-
+const content = `
+  <article class="backpack" id="everyday">
+    <figure class="backpack__image">
+      <img src=${everydayPack.image} alt="" />
+    </figure>
+    <h1 class="backpack__name">${everydayPack.name}</h1>
+    <ul class="backpack__features">
+      <li class="packprop backpack__volume">Volume:<span> ${
+        everydayPack.volume
+      }l</span></li>
+      <li class="packprop backpack__color">Color:<span> ${
+        everydayPack.color
+      }</span></li>
+      <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
+      <li class="packprop backpack__pockets">Number of pockets:<span> ${
+        everydayPack.pocketNum
+      }</span></li>
+      <li class="packprop backpack__strap">Left strap length:<span> ${
+        everydayPack.strapLength.left
+      } inches</span></li>
+      <li class="packprop backpack__strap">Right strap length:<span> ${
+        everydayPack.strapLength.right
+      } inches</span></li>
+      <li class="packprop backpack__lid">Lid status:<span> ${
+        everydayPack.lidOpen
+      }</span></li>
+    </ul>
+  </article>
+`;
 
 console.log("The everydayPack object: ", everydayPack);
 console.log("The pocketNum value: ", everydayPack.pocketNum);
