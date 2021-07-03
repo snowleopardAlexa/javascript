90bbdaaf285c17f46260818184fca8a4b7f8e341
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-02 20:11:01 -0400

    add const everydayPack and console.log

--- backpack-script.js
+++ backpack-script.js
@@ -5,3 +5,14 @@
 
 import Backpack from './Backpack.js';
 
+const everydayPack = new Backpack (
+    "Everyday Backpack",
+    30,
+    "grey",
+    15,
+    26,
+    false
+);
+
+console.log("The everydayPack object: ", everydayPack);
+console.log("The pocketNum value: ", everydayPack.pocketNum);
