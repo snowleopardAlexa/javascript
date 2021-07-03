0a220bf6cfbed9cef5c9516c5bbaad66f59d77bf
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-02 20:21:21 -0400

    define properties

--- Backpack.js
+++ Backpack.js
@@ -16,7 +16,16 @@ class Backpack {
         strapLengthR,
         lidOpen
     ) {
-        
+        // define properties:
+        this.name = name;
+        this.volume = volume;
+        this.color = color;
+        this.pocketNum = pocketNum;
+        this.strapLength = {
+            left: strapLengthL,
+            right: strapLengthR,
+        };
+        this.lidOpen = lidOpen;
     
     }
 }
