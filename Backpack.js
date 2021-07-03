593a8dd365299c899a8b88b9dc65b5fb7edcdfdb
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-02 20:19:33 -0400

    create class backpack and define parameters

---
+++ Backpack.js
@@ -0,0 +1,22 @@
+/**
+ * Creating classes:
+ * 
+ * Class declaration: class Name {}
+ * Class expression: const Name = class {}
+ */
+
+class Backpack {
+    constructor (
+        // defines parameters:
+        name,
+        volume,
+        color,
+        pocketNum,
+        strapLengthL,
+        strapLengthR,
+        lidOpen
+    ) {
+        
+    
+    }
+}
