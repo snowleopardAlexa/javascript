3bfedb0dc2740b3355c85e8773ee41c55c38d30b
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-02 20:45:05 -0400

    add new method backpackAge

--- Backpack.js
+++ Backpack.js
@@ -36,6 +36,13 @@ class Backpack {
         this.strapLength.left = lengthLeft;
         this.strapLength.right = lengthRight;
     }
+    backpackAge() {
+        let now = new Date();
+        let acquired = new Date(this.dateAcquired);
+        let elapsed = now - acquired; // elapsed time in seconds
+        let daysSinceAcquired = Math.floor(elapsed / (1000 * 3600 * 24));
+        return daysSinceAcquired;
+    }
 }
 
 export default Backpack;
