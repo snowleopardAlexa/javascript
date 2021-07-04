abc89774bb26e0af3d5d2ac38fa4fbbc673b5a3e
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-04 15:02:41 -0400

    add math operators

---
+++ math-operators/script.js
@@ -0,0 +1,16 @@
+/**
+ * Arithmetic operators
+ */
+
+let a = 15;
+let b = 4;
+let c = 3;
+
+console.log(`let a: ${a} (${typeof a})`);
+console.log(`let b: ${b} (${typeof b})`);
+console.log(`let c: ${c} (${typeof c})`);
+
+let result = a % b;
+
+console.log("Result", result);
+
