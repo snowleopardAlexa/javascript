d6efa76b33a3dac544897bb38b3d16b96f6130bf
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-04 15:08:04 -0400

    add plus warning

--- math-operators/script.js
+++ math-operators/script.js
@@ -12,5 +12,18 @@ console.log(`let c: ${c} (${typeof c})`);
 
 let result = a % b;
 
+/**
+ * Addition
+ * let a = 15;
+   let b = "4";
+   let c = 3;
+
+   let result = a + b;
+   console: 154 
+
+   Be careful with addition, while JS will still read "4" as the number when we do subtraction,
+   divination, and multipling, it will act weird with PLUS.
+ */
+
 console.log("Result", result);
 
