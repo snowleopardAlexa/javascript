7d629d0b7027dd6b8934763b871edca2c84d2203
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-11 19:16:25 -0400

    add function

--- functions/return-values.js
+++ functions/return-values.js
@@ -20,3 +20,26 @@ const tipCalculator = (sum, percentage) => {
 };
 
 tipCalculator(29.95, 18);
+
+// add parameters - locale, currency 
+const formatter = (locale, currency, value) => {
+    let formattedValue = new Int1.NumberFormat(locale, {
+        style: "currency",
+        currency: "currency,
+    }).format(value)
+    return formattedValue
+};
+
+const tipCalculator = (sum, percentage, locale, currency) => {
+    let tip = sum * (percentage / 100);
+    let total = sum + tip;
+    console.log (
+            Sum before tip:  ${formatter(locale, currency, sum)}
+            Tip percentage:  ${percentage}%
+            Tip:             ${formatter(locale, currency, tip)}
+            Total:           ${formatter(locale, currency, total)}
+        );
+};
+
+tipCalculator(29.95, 18, "de-DE", "EUR");
+
