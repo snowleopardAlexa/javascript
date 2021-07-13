03f33973a6ef70d95e352fadc3604612a116bb9a
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-12 20:15:27 -0400

    add function and function body

--- functions/script.js
+++ functions/script.js
@@ -30,4 +30,14 @@ const greenPack = {
     pocketNum: 3,
 };
 
-
+const addPack = function (currentPack) {
+    const newArticle = document.createElement("article");
+    newArticle.innerHTML = `
+    <h1>${currentPack.name}</h1>
+    <ul>
+        <li>Volume: ${currentPack.volume}</li>
+        <li>Color: ${currentPack.volume}</li>
+        <li>Number of pockets: ${currentPack.pocketNum}</li>
+    </ul>
+    `;
+}
