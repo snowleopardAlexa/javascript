e2f073ef7af589b25c5177b4480f3c1efe8c2009
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-13 18:51:02 -0400

    add switch statement

--- functions/switch-statement.js
+++ functions/switch-statement.js
@@ -75,3 +75,36 @@ const content = `
          };
 
          usedStatus()
+
+// use SWITCH statement instead of multiple if statements for better code performance
+    const usedStatus = () => {
+    let age = everydayPack.backpackAge();
+    age = 1095
+    let description;
+
+    switch (true) {
+        case age < 30:
+            description = "new"
+            break;
+        case age >= 30 && age < 365:
+            description = "lightly used"
+            break;
+        case age >= 365 && age < 1095:
+            description = "used"
+            break;
+        case age >= 1095:
+            description = "old"
+            break;
+        default:
+            console.log(`There is no description for ${age}.`)                
+    }
+
+    console.log (`
+    Age: ${age} days
+    Status: ${description}
+    `);
+   };
+
+   usedStatus()
+
+
