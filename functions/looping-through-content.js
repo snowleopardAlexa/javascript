a616b223b0b98c208b191adca8ce7bdff93ee39b
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-13 18:59:51 -0400

    looping through array of objects

---
+++ functions/looping-through-content.js
@@ -0,0 +1,40 @@
+// Looping through a content
+
+const stuff = ["piggy", "headlamp", "pen", "pencil", "eraser", "water bottle"];
+
+const nestedObjects = {
+    item01: {
+        name: "piggy",
+        type: "toy",
+        weight: 30,
+    },
+    item02: {
+        name: "headlamp",
+        type: "equipment",
+        weight: 120,
+      },
+      item03: {
+        name: "pen",
+        type: "tool",
+        weight: 30,
+      },
+      item04: {
+        name: "pencil",
+        type: "tool",
+        weight: 30,
+      },
+      item05: {
+        name: "eraser",
+        type: "tool",
+        weight: 40,
+      },
+      item03: {
+        name: "water bottle",
+        type: "equipment",
+        weight: 1300,
+      },
+};
+
+const article = document.querySelector("article");
+let stuffList = document.createElement("ul");
+
