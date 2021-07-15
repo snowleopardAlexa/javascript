7120906fd9bedd996087179f43cd8078d2d6db04
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-14 21:09:18 -0400

    add arrays and for loop

---
+++ CHALLENGES/two.js
@@ -0,0 +1,19 @@
+// Create an array and loop through it using for loop
+
+// Array
+const dogs = ["German Shepherd", "Golden Retriever", "Dashhund"];
+
+// for loop
+for (let dog = 0; dog < dogs.length; dog++) {
+    console.log(dogs[dog]);
+}
+
+// create an array of cats and loop through it
+const cats = ["bobcat", "lion", "leopard"];
+
+for (let cats = 0; cat < cats.length; cat++) {
+    console.log(cats[cat]);
+}
+
+
+
