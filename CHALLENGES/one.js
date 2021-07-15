e8176141646a0167fb3f94e683ba34a505d7b331
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-14 20:27:51 -0400

    add multiply, subtract, add, divide functions

---
+++ CHALLENGES/one.js
@@ -0,0 +1,32 @@
+// Multiply, add, subtract, divide function
+
+// Multiply function
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+let multiplySum = multiply(23,45);
+console.log(multiplySum);
+
+// Add function
+const addCats = (bobcat, lion) => {
+    return bobcat + lion;
+}
+let cats = addCats(12,45);
+console.log(cats);
+
+// Subtract function
+const subtractRats = (whiteRats, blackRats) => {
+    return whiteRats - blackRats;
+}
+let rats = subtractRats(34,56);
+console.log(rats);
+
+// Divide function
+const divideBirds = (swan, pelican) => {
+    return swan / pelican;
+}
+let birds = divideBirds(45,5);
+console.log(birds);
+
+
+
