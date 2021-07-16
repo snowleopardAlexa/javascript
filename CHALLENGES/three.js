730a71fc7de24ddf0f2da8e5191a4ed5bc2ba46a
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-16 09:23:49 -0400

    add ternary operator enhanced readability

--- CHALLENGES/three.js
+++ CHALLENGES/three.js
@@ -87,6 +87,9 @@ animal === "cat"
 ? console.log("You will be a cat herder")
 : console.log("You will be a dog catcher");
 
+// ternary operator --> enhanced readability
+var job = (animal === "cat") ? ("cat herder") : ("dog catcher");
+
 
 
 
