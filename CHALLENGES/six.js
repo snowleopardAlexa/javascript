0df1e8f11e53830e9d74cd7e72b8f53e21277fe8
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-17 16:37:40 -0400

    add ternery with while

--- CHALLENGES/six.js
+++ CHALLENGES/six.js
@@ -36,4 +36,18 @@ let marks = prompt('Enter your marks :');
 let result = (marks >= 40) ? 'pass' : 'fail';
 console.log(`You ${result} the exam.`);
 
+// ternary operator with while
+const numOfBottles = 10;
+
+while(numOfBottles > 0) {
+    const bottles = numOfBottles < 2 ? "bottle" : "bottles";
+    console.log(numOfBottles + "" + bottles + " of beer on the wall.");
+    console.log(numOfBottles + " " + bottles + " of beer,");
+    console.log("take one down, pass it around,");
+    numOfBottles--;
+
+    console.log(numOfBottles + " " + bottles + " of beer on the wall.");
+}
+
+
 
