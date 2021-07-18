6b99f1842a4942425e209112e04f8ba735d5f2bd
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-18 15:26:05 -0400

    add var

--- CHALLENGES/five.js
+++ CHALLENGES/five.js
@@ -1,10 +1,18 @@
 // Variables
 
 // var is use for variable in js problem with it that it has
-// complicated scope
+// complicated scope, it is dynamic, it can be changed
 var varExample = "var"; 
 // const is use for constant values as this
-// variable scope is same as let
+// variable scope is same as let, but it is NOT dynamic
 const constExample = "const";
-// let variable is like const but it is dynamic
+// let variable is like const with a scope and like var - it is dynamic
 let letExample = "let";
+
+// var
+var Number = 5;
+var String = "Hi!";
+var boolen1 = true;
+var boolen2 = false;
+var array = [11, "Hi!", true];
+var object = {age:11, speach:"Hi!", likes_Bananas:true};
