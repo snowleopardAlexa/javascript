b3c11a6a44af8f503f3415743f3b94e1e9680fba
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-18 15:12:30 -0400

    add template literals

--- CHALLENGES/four.js
+++ CHALLENGES/four.js
@@ -2,4 +2,8 @@
 const age = 4
 console.log(`I'm ${age} years old!`)
 
+const animal = "cow";
+const desc = `The ${animal} jumped over the moon`;
+console.log(desc);
 
+// Regular string vs template literals
