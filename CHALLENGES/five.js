584ca3c8bddcdc3dde7b44de358f7a28e3af3c97
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-18 15:22:58 -0400

    add variables

---
+++ CHALLENGES/five.js
@@ -0,0 +1,10 @@
+// Variables
+
+// var is use for variable in js problem with it that it has
+// complicated scope
+var varExample = "var"; 
+// const is use for constant values as this
+// variable scope is same as let
+const constExample = "const";
+// let variable is like const but it is dynamic
+let letExample = "let";
