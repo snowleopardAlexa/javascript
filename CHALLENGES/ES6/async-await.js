a4170fc4867869fb9be7f9d3814e31d409e2b26d
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-19 16:41:17 -0400

    add incorporating fetch with async/await

--- CHALLENGES/ES6/async-await.js
+++ CHALLENGES/ES6/async-await.js
@@ -19,3 +19,13 @@ const countToFive = async() => {
 
 countToFive();
 
+// Incorporating fetch with async/await
+const githubRequest = async (login) => {
+    let response = await fetch(`https://api.github.com/users/${login}`
+    );
+    let json = await response.json();
+    let summary = `${json.name}, ${json.company}`;
+    console.log(summary);
+};
+
+githubRequest("alexaslomski")
