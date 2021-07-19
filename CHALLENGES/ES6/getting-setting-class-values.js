f0083fb1c69e8ad8e0b5c18c43953045b3d4ac52
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-19 13:48:34 -0400

    add getter and setter in class

--- CHALLENGES/ES6/getting-setting-class-values.js
+++ CHALLENGES/ES6/getting-setting-class-values.js
@@ -15,3 +15,20 @@ console.log(attendance.list);
 attendance.addName = "Bill Benkelman";
 attendance.addName = "Charlie Charslon";
 console.log(attendance.list);
+
+// getter and setter in class
+class Hike {
+    constructor(distance, pace) {
+        this.distance = distance;
+        this.pace = pace;
+    }
+    get lengthInHours() {
+        return `${this.calcLength()} hours`;
+    }
+    calcLength() {
+        return this.distance / this.pace;
+    }
+}
+
+const mtTallac = new Hike(10,2);
+console.log(mtTallac.lengthInHours);
