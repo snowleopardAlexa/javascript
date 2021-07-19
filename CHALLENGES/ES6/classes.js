afc512ffe2933cd99726e31d60a641b0a8f645a6
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-19 13:36:59 -0400

    add inheritance with JS classes

--- CHALLENGES/ES6/classes.js
+++ CHALLENGES/ES6/classes.js
@@ -14,3 +14,13 @@ class Vehicle {
   console.log(coolSkiVan);
   // we don't need to repeat console.log
   coolSkiVan.describeYourself();
+
+  // Inheritance with JS classes
+  class SemiTruck extends Vehicle {
+      constructor() {
+          super("semi truck", 18);
+      }
+  }
+
+  let groceryStoreSemi = new SemiTruck();
+  groceryStoreSemi.describeYourself()
