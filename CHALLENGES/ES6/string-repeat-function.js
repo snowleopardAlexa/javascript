313af43d757a4d5a53cddfd864509a8fb961d688
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-19 13:54:24 -0400

    add string repeat function

---
+++ CHALLENGES/ES6/string-repeat-function.js
@@ -0,0 +1,22 @@
+// Using string repeat function
+let yell = "woo!";
+
+let party = yell.repeat(20);
+console.log(party);
+
+let cat = {
+    meow(times) {
+        console.log("meow".repeat(times));
+    }
+    purr(times) {
+        console.log("prrr".repeat(times))
+    },
+    snore(times) {
+        console.log("ZzZZ".repeat(times));
+    }
+};
+
+cat.meow(3);
+cat.purr(3);
+cat.snore(6);
+
