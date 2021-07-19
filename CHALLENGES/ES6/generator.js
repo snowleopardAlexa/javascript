ae2bee63f98c4b7abae73cd0f2829ed9b95928a0
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-19 16:10:11 -0400

    add generators

---
+++ CHALLENGES/ES6/generator.js
@@ -0,0 +1,16 @@
+// generator
+function director() {
+  yield "Three";
+  yield "Two";
+  yield "One";
+  yield "Action";
+}
+
+let countdown = director();
+
+console.log(countdown.next().value);
+console.log(countdown.next().value);
+console.log(countdown.next().value);
+console.log(countdown.next());
+console.log(countdown.next());
+
