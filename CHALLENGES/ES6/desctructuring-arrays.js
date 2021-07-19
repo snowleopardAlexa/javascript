0b7375ea8a0a64d98cbd28f3388f81068a953391
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-19 12:49:31 -0400

    destructuring array

---
+++ CHALLENGES/ES6/desctructuring-arrays.js
@@ -0,0 +1,13 @@
+// Destructuring array
+// , , , , --> represent 4 elements in the array, they will be printed out too
+// it's shortcut, instead of typing multiple console.log(second) etc.
+let [first, , , , fifth] = [
+    "NYC",
+    "LA",
+    "Seatlle",
+    "JC",
+    "Corona"
+];
+
+console.log(first);
+console.log(fifth);
