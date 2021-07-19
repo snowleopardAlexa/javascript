f46831a9857e438d7cb3016b5510b6d68bc8e3d3
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-19 16:16:23 -0400

    promises

---
+++ CHALLENGES/ES6/promises.js
@@ -0,0 +1,17 @@
+// PROMISES
+
+const delay = (seconds) =>
+  new Promise((resolve, reject) => {
+      if (typeof seconds !== "number") {
+          reject (
+              new Error("seconds must be a number!")
+          );
+      }
+
+    setTimeout(resolve, seconds * 1000);
+  });
+
+  console.log("zero seconds");
+  delay(1).then(() => console.log("1 sec"));
+  delay(5).then(() => console.log("5 secs"));
+  
