a3302a5d28278d171adf6cc568af7ee95f8a38aa
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-19 16:26:33 -0400

    add loading remote data with promise

--- CHALLENGES/ES6/promises.js
+++ CHALLENGES/ES6/promises.js
@@ -1,5 +1,6 @@
 // PROMISES
 
+// promise 
 const delay = (seconds) =>
   new Promise((resolve, reject) => {
       if (typeof seconds !== "number") {
@@ -14,4 +15,27 @@ const delay = (seconds) =>
   console.log("zero seconds");
   delay(1).then(() => console.log("1 sec"));
   delay(5).then(() => console.log("5 secs"));
-  
+
+  // Loading remote data with promises
+  const spacePeople = () => {
+      return new Promise((resolves, rejects) => {
+          const api = "http://api.open-notify.org/astro.json";
+          const request = new XMLHttpRequest();
+          request.open("GET", api);
+          request.onload = () => {
+              if (request.status === 200) {
+                  resolves(JSON.parse(request.response));
+              } else {
+                  rejects(Error(request.statusText));
+              }
+          }; 
+          request.oneerror = (err) => rejects(err);
+          request.send();
+      });
+  };
+
+  spacePeople().then(
+      (spaceData) => console.log(spaceData),
+      (err) => 
+        console.error(new Error("Can't load people"))
+  );
