44b07b488cd168fed5d9385c118c518671e6b9be
snowleopardAlexa <alexaslomski@gmail.com>
2021-07-23 09:33:21 -0400

    add json example

---
+++ JSON/json.js
@@ -0,0 +1,10 @@
+// JSON - JavaScript Object Notation
+// format for sharing data
+// derived from JS
+// language independent
+// an alternate to XML
+// easy to read
+// leaner than XML
+// growing support in APIs -> Twitter
+// natural format of JS
+// implementation in many languages
---
+++ JSON/objects-json.js
@@ -0,0 +1,19 @@
+// JavaScript and JSON
+
+// JSON STRINGS are in DOUBLE QUOTES
+{ "full_name" : "Alexa Slomski" }
+// JSON keys wrapped in "quotes"
+// JSON keys can be any valid string
+// JSON values have to be one of six data types:
+// strings, numbers, objects, arrays, booleans, or null
+
+// JS OBJECTS
+// JavaScript Objects - can be any data types, including a function
+var info { full name : "Alexa Slomski" };
+// quotes on keys not necessary
+var info = {
+    full_name: "Alexa",
+    getName: function() {
+        alert(this.full_name);
+    }
+};
